385619399842043710d86bb6017b2d0acf62d112
Jonathan Delgado Zamorano <jonad.correo@gmail.com>
2014-08-13 00:16:04 -0400

    mié ago 13 00:16:04 CLT 2014

--- src/show-tell.js
+++ src/show-tell.js
@@ -1,5 +1,42 @@
-!$(document).delegate('[data-show-tell]','click',function(e){
+!$(document).delegate('[data-show-tell-arun]','click',function(e){
+	e.preventDefault();
 
-	$($(this).data("show-tell"),this).show();
+	var prt = $(this).parent('[data-show-tell]');
 
+	var fnd  = $(prt).data("show-tell");
+	var clss = $(prt).data("show-tell-rmclass");
+
+	var elms = $(fnd,prt);
+
+	if (clss) {
+		$(elms).removeClass(clss);
+		$(elms).show();
+	} else {
+		$(elms).show();
+	}
+
+	$(elms).attr('show-tell-active',true);
+
+	$(prt).trigger('showTell.focus');
+});
+!$(document).delegate('[data-show-tell-reverse]','click',function(e){
+	e.preventDefault();
+	
+	var prt = $(this).parent('[data-show-tell]');
+
+	var fnd  = $(prt).data("show-tell");
+	var clss = $(prt).data("show-tell-rmclass");
+
+	var elms = $('[show-tell-active]',prt);
+
+	if (clss) {
+		elms.addClass(clss);
+		elms.hide();
+	} else {
+		elms.hide();
+	}
+
+	elms.removeAttr('show-tell-active');
+
+	$(prt).trigger('showTell.close');
 });
