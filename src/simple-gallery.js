ea78bd872667602f7e53eddcb5819781c4d6187f
Jonathan Delgado Zamorano <jonad.correo@gmail.com>
2014-08-26 16:02:32 -0400

    mar ago 26 16:02:32 CLT 2014

--- src/simple-gallery.js
+++ src/simple-gallery.js
@@ -13,3 +13,51 @@ $(document).delegate('[data-gallery-miniature]','click',function(e){
 		return false;
 	};
 });
+
+$(document).delegate('[data-gallery]','onNext',function(e){
+	var gGallery = $(this);
+	var listMiniature = $('[data-gallery-list]',gGallery);
+	var cover = $('[data-gallery-cover]',gGallery);
+	var srcThisCover = cover.attr('src');
+	var miniatureThisCover = $('[data-gallery-miniature][data-href="'+srcThisCover+'"]',listMiniature);
+	var nextListMiniature = miniatureThisCover.parents('[data-gallery-list]').next();
+	var nextMiniature = $("[data-gallery-miniature]",nextListMiniature);
+
+	if (nextMiniature.length >= 1) {
+		nextMiniature.trigger( "click" );
+	} else {
+		$('[data-gallery-miniature]',listMiniature[0]).trigger( "click" );
+	}
+});
+
+$(document).delegate('[data-gallery]','onPrev',function(e){
+	var gGallery = $(this);
+	var listMiniature = $('[data-gallery-list]',gGallery);
+	var cover = $('[data-gallery-cover]',gGallery);
+	var srcThisCover = cover.attr('src');
+	var miniatureThisCover = $('[data-gallery-miniature][data-href="'+srcThisCover+'"]',listMiniature);
+	var prevListMiniature = miniatureThisCover.parents('[data-gallery-list]').prev();
+	var prevMiniature = $("[data-gallery-miniature]",prevListMiniature);
+
+	if (prevMiniature.length >= 1) {
+		prevMiniature.trigger( "click" );
+	} else {
+		$('[data-gallery-miniature]',listMiniature[listMiniature.length-1]).trigger( "click" );
+	}
+});
+
+$(document).delegate("[data-gallery-next]",'click',function(e){
+	var min = $(this);
+	var gGallery  = min.parents("[data-gallery]")[0];
+	if (gGallery) {
+		$(gGallery).trigger('onNext');
+	}
+});
+
+$(document).delegate("[data-gallery-prev]",'click',function(e){
+	var min = $(this);
+	var gGallery  = min.parents("[data-gallery]")[0];
+	if (gGallery) {
+		$(gGallery).trigger('onPrev');
+	}
+});
